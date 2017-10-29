a42bc650d884ef074c453ebf3c16711dd0e56081
juan mateo <juanmateocapo@gmail.com>
2017-10-29 18:26:04 -0300

    INICIO

---
+++ MAIN.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   MAIN.cpp
+ * Author: juan
+ *
+ * Created on October 29, 2017, 6:15 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
